025e8e0ee569c2d2de721731e5358fa9bff6be6a
Martin <mkmartinoes@gmail.com>
2022-04-08 13:25:18 +0300

    test: receive a callback function

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const receivesAFunction = (a) => {
     return a();
 }
+
+function justAfunction (){
+    return "''"
+}
+
+var fn = returnsANamedFunction();
+function returnsANamedFunction() {
+    return function view(){ return ""}
+}
+
+// const returnsANamedFunction = function(name = function hello(){}) {
+//     let fn;
+//     fn = name();
+// } 
+
+const returnsAnAnonymousFunction = () => {
+ return function views(){return ""}
+}
+
+var fn = returnsAnAnonymousFunction()
